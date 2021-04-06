aa6c2a36092b6f4997b5833c56056abea5a12866
Johnny Nguyen <vietnguyen09@me.com>
2021-04-06 16:26:40 +0700

    Refactor test

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { AddExpensePage } from '../../components/AddExpensePage';
+import expenses from '../fixtures/expenses';
+
+
+let onSubmit, history, wrapper;
+beforeEach(() => {
+    onSubmit = jest.fn();
+    history = { push: jest.fn() };
+    wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+});
 
 test('should render add expense page correctly', () => {
-    const onSubmit = jest.fn();
-    const history = { push: jest.fn() };
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
     expect(wrapper).toMatchSnapshot();
+});
+
+test('should handler onSubmit', () => {
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
+    expect(history.push).toHaveBeenLastCalledWith('/');
+    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
 });
